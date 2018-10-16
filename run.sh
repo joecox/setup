3236afffa51d603d00351566856090e2c32a9928
Joe Cox <joe@factual.com>
2018-10-15 23:32:51 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,13 +22,19 @@ brew install emacs \
              gzip \
              jq \
              leiningen \
-             nvm \
              pstree \
-             ripgrep-bin \
+             ripgrep \
              tree \
              vault \
              yarn \
-             z
+             z \
+             zsh
+             
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+             
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # Install brew-cask things
 brew cask install docker \
@@ -36,5 +42,4 @@ brew cask install docker \
                   keybase
 
 # TODO: setup z caveats in zshrc
-# TODO: maybe use official nvm installation + zshrc setup
 # TODO: setup new ssh key with github
