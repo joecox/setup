4c727f1f4fa0e4ba609ac36f9d07218ddb0a30de
Joe Cox <joe@factual.com>
2018-10-16 00:09:46 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,6 +14,13 @@ if test ! $(which brew); then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# Setup shell
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir -p ~/.config
+echo 'export XDG_CONFIG_HOME="$HOME/.config"' >> ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/joecox/setup/master/com.googlecode.iterm2.plist > ~/.config/com.googlecode.iterm2.plist
+
 # Install brew things
 brew install gnu-sed --with-default-names
 brew install emacs \
@@ -27,17 +34,14 @@ brew install emacs \
              tree \
              vault \
              yarn \
-             z \
-             zsh
-             
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-             
+             z
+
 # Install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # Install brew-cask things
 brew cask install docker \
+                  flux \
                   iterm2 \
                   keybase
 
